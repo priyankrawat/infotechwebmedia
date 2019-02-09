fb5bac1fd99e3685e70c3e101a821a1820eaa2e0
priyankrawat <30529816+priyankrawat@users.noreply.github.com>
2019-02-09 20:18:27 +0530

    Add files via upload

---
+++ mywebsite.php
@@ -0,0 +1,487 @@
+<?php include_once "insert.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>InfotechWebMedia</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+
+
+
+  <!--bootstrap and css links-->
+ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+
+
+
+
+  <!-- Favicons -->
+  <link href="img/favicon.ico" rel="icon">
+  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Bootstrap CSS File -->
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
+  <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="css/style.css" rel="stylesheet">
+  <link rel="stylesheet" href="form.css" >
+        <script src="form.js"></script>
+</head>
+
+<body id="page-top">
+
+  <!--/ Nav Star /-->
+  <nav class="navbar navbar-b navbar-trans navbar-expand-md fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll" href="#page-top">InfotechWebMedia</a>
+      <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarDefault"
+        aria-controls="navbarDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+      <div class="navbar-collapse collapse justify-content-end" id="navbarDefault">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link js-scroll active" href="#home">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll" href="#about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll" href="#service">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll" href="#work">Work</a>
+          </li>
+      
+          <li class="nav-item">
+            <a class="nav-link js-scroll" href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!--/ Nav End /-->
+
+  <!--/ Intro Skew Star /-->
+  <div id="home" class="intro route bg-image" style="background-image: url(img/intro-bg.jpg)">
+    <div class="overlay-itro"></div>
+    <div class="intro-content display-table">
+      <div class="table-cell">
+        <div class="container">
+          <!--<p class="display-6 color-d">Hello, world!</p>-->
+          <h1 class="intro-title mb-4">Infotech Web Media</h1>
+          <p class="intro-subtitle"><span class="text-slider-items">Website Development,Search Engine Optimization,Social Media Marketing,Website Redesign</span><strong class="text-slider"></strong></p>
+
+          <!-- <p class="pt-3"><a class="btn btn-primary btn js-scroll px-4" href="#about" role="button">Learn More</a></p> -->
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--/ Intro Skew End /-->
+
+
+
+
+
+<!--==========================
+      About Us Section
+    ============================-->
+
+ <section id="about">
+      <div class="container">
+
+        <header class="section-header">
+          <div class="title-box text-center">
+            <h3 class="title-a">
+            About Us</h3>
+          <h4 style="font-family: 'Comic Sans MS', cursive, sans-serif">We provide the best internet service you ever want. Whether it is creating your own website or increasing your Google,Yahoo,Bing search ranking.We also provide social media marketing services such as views,likes,comments,followers for Facebook, Instagram, Twitter, Youtube. All of our services are genuine and we dont use bots </h4>
+          <div class="line-mf"></div>
+        </div>
+        </header>
+      
+
+
+        <div class="row about-cols">
+
+          <div class="col-md-4 wow fadeInUp" style="visibility: visible; animation-name: fadeInUp;">
+            <div class="about-col">
+              <div class="img">
+                <img src="img/about-mission.jpg" alt="" class="img-fluid">
+                <div class="icon"><i class="ion-ios-speedometer-outline"></i></div>
+              </div>
+              <h2 class="title"><a href="#">Our Mission</a></h2>
+              <p>
+                Our highly qualified team provides the best internet services as per the customer needs on time.
+                
+              </p>
+            </div>
+          </div>
+
+          <div class="col-md-4 wow fadeInUp" data-wow-delay="0.1s" style="visibility: visible; animation-delay: 0.1s; animation-name: fadeInUp;">
+            <div class="about-col">
+              <div class="img">
+                <img src="img/about-plan.jpg" alt="" class="img-fluid">
+                <div class="icon"><i class="ion-ios-list-outline"></i></div>
+              </div>
+              <h2 class="title"><a href="#">Our Plan</a></h2>
+              <p>
+                We use strategies which boosts your social media profiles.We dont use bots for increasing views,followers,likes,comments or subscribers.
+              </p>
+            </div>
+          </div>
+
+          <div class="col-md-4 wow fadeInUp" data-wow-delay="0.2s" style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp;">
+            <div class="about-col">
+              <div class="img">
+                <img src="img/about-vision.jpg" alt="" class="img-fluid">
+                <div class="icon"><i class="ion-ios-eye-outline"></i></div>
+              </div>
+              <h2 class="title"><a href="#">Our Vision</a></h2>
+              <p>
+                Our team works as per the demands of the customer discussing briefly our strategies and how the work wil be done.
+              </p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section>
+
+<!-- #about -->
+
+
+
+
+
+
+  <!--/ Section Services Star /-->
+  <section id="service" class="services-mf route">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="title-box text-center">
+            <h3 class="title-a">
+              Services
+            </h3>
+            <h4 style="font-family: 'Comic Sans MS', cursive, sans-serif">
+              WE CREATE CREATIVE & BEST UNIQUE DESIGN.LETS WORK TOGETHER.
+            </h4>
+            <div class="line-mf"></div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4">
+          <div class="service-box">
+            <div class="service-ico">
+              <span class="ico-circle"><i class="ion-monitor"></i></span>
+            </div>
+            <div class="service-content">
+              <h2 class="s-title">Web Design</h2>
+              <p class="s-description text-center">
+                If you want an ecommerce website or want to post blogs or want to build any kind of professional website then you are at a right place.We can also redesign your old websites.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="service-box">
+            <div class="service-ico">
+              <span class="ico-circle"><i class="ion-code-working"></i></span>
+            </div>
+            <div class="service-content">
+              <h2 class="s-title">Search Engine Optimisation (SEO)</h2>
+              <p class="s-description text-center">
+                If your website is not indexed on major search engines like Google,yahoo then we can help you in ranking up your website, providing genuine visitors. 
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="service-box">
+            <div class="service-ico">
+              <span class="ico-circle"><i class="ion-camera"></i></span>
+            </div>
+            <div class="service-content">
+              <h2 class="s-title">Social Media Boost</h2>
+              <p class="s-description text-center">
+                We boost your social media profiles whether its Youtube, Facebook, Instagram, Tik-Tok.We provide genuine followers, likes, subscribers, comments making you famous in very short time.
+              </p>
+            </div>
+          </div>
+        </div>
+        </div>
+      </div>
+    </section>
+
+  <!--/ Section Services End /-->
+
+
+
+
+ <!--/ Section Portfolio Star /-->
+
+  <section id="work" class="portfolio-mf sect-pt4 route">
+  <div class="section-counter paralax-mf bg-image" style="background-image: url(img/counters-bg.jpg)">
+    <div class="overlay-mf"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-3 col-lg-3">
+          <div class="counter-box">
+            <div class="counter-ico">
+              <span class="ico-circle"><i class="ion-checkmark-round"></i></span>
+            </div>
+            <div class="counter-num">
+              <p class="counter">12750</p>
+              <span class="counter-text">WORKS COMPLETED</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-3 col-lg-3">
+          <div class="counter-box pt-4 pt-md-0">
+            <div class="counter-ico">
+              <span class="ico-circle"><i class="ion-ios-calendar-outline"></i></span>
+            </div>
+            <div class="counter-num">
+              <p class="counter">5</p>
+              <span class="counter-text">YEARS OF EXPERIENCE</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-3 col-lg-3">
+          <div class="counter-box pt-4 pt-md-0">
+            <div class="counter-ico">
+              <span class="ico-circle"><i class="ion-ios-people"></i></span>
+            </div>
+            <div class="counter-num">
+              <p class="counter">4578</p>
+              <span class="counter-text">TOTAL CLIENTS</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-3 col-lg-3">
+          <div class="counter-box pt-4 pt-md-0">
+            <div class="counter-ico">
+              <span class="ico-circle"><i class="ion-ribbon-a"></i></span>
+            </div>
+            <div class="counter-num">
+              <p class="counter">70</p>
+              <span class="counter-text">Team Members</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </section>
+ 
+    <!--/ Section Portfolio End /-->
+
+  
+  
+
+  
+  <section id="" class="blog-mf sect-pt4 route">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="title-box text-center">
+            <h3 class="title-a">
+              
+            </h3>
+            <p class="subtitle-a">
+              <h1 style="font-family: 'Comic Sans MS', cursive, sans-serif">" We are an established and trusted web agency with a reputation for commitment and high integrity. "</h1>
+            </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+  <!--/ Section Blog End /-->
+
+  <!--/ Section Contact-Footer Star /-->
+  <section class="paralax-mf footer-paralax bg-image sect-mt4 route" style="background-image: url(img/overlay-bg.jpg)">
+    <div class="overlay-mf"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="contact-mf">
+            <div id="contact" class="box-shadow-full">
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="title-box-2">
+                    <h5 class="title-left">
+                      Send Message Us
+                    </h5>
+                  </div>
+                  
+
+
+
+
+
+
+
+
+
+
+
+<div class="container" style="width:500px;">  
+                <form id="submit_form">  
+                     <label>Username</label>  
+                     <input type="text" name="username" id="username" class="form-control" />  
+                     <br /> 
+                     <label>User Email</label>  
+                     <input type="email" id="email" class="form-control" /> 
+                     <br />  
+                      <label>Mobile Number</label>  
+                     <input type="phone" id="phone" class="form-control" /> 
+                     <br /> 
+                     <label>Message</label>  
+                     <textarea name="message" id="message" class="form-control"></textarea>  
+                     <br />  
+
+                     <input type="button" name="submit" id="submit" class="btn btn-info" value="Submit" />  
+                     <span id="error_message" class="text-danger"></span>  
+                     <span id="success_message" class="text-success"></span>  
+                </form>  
+           </div>  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+               </div>
+
+                <div class="col-md-6">
+                  <div class="title-box-2 pt-4 pt-md-0">
+                    <h5 class="title-left">
+                      Get in Touch
+                    </h5>
+                  </div>
+                  <div class="more-info">
+                    <p class="lead">
+                      If you want to talk to us feel free to contact .
+                    </p>
+                    <ul class="list-ico">
+                      <li><span class="ion-ios-location"></span> 178 M 18 NEW DELHI, India</li>
+                      <li><span class="ion-ios-telephone"></span> (+91) 7838687811</li>
+                      <li><span class="ion-email"></span>contact@infotechwebmedia.com</li>
+                    </ul>
+                  </div>
+                  <div class="socials">
+                    <ul>
+                      <li><a href="www.facebook.com"><span class="ico-circle"><i class="ion-social-facebook"></i></span></a></li>
+                      <li><a href="www.instagram.com"><span class="ico-circle"><i class="ion-social-instagram"></i></span></a></li>
+                      <li><a href="www.twitter.com"><span class="ico-circle"><i class="ion-social-twitter"></i></span></a></li>
+                      <li><a href="www.pinterest.com"><span class="ico-circle"><i class="ion-social-pinterest"></i></span></a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <div class="copyright-box">
+              <p class="copyright">&copy; Copyright <strong>InfotechwebMedia Pvt Ltd</strong>. All Rights Reserved</p>
+              <div class="credits">
+                
+                Designed by <a href="https://facebook.com/">InfotechwebMedia Pvt Ltd</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </section>
+  <!--/ Section Contact-footer End /-->
+
+  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+  <div id="preloader"></div>
+
+  <!-- JavaScript Libraries -->
+  <script src="lib/jquery/jquery.min.js"></script>
+  <script src="lib/jquery/jquery-migrate.min.js"></script>
+  <script src="lib/popper/popper.min.js"></script>
+  <script src="lib/bootstrap/js/bootstrap.min.js"></script>
+  <script src="lib/easing/easing.min.js"></script>
+  <script src="lib/counterup/jquery.waypoints.min.js"></script>
+  <script src="lib/counterup/jquery.counterup.js"></script>
+  <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+  <script src="lib/lightbox/js/lightbox.min.js"></script>
+  <script src="lib/typed/typed.min.js"></script>
+  <!-- Contact Form JavaScript File -->
+  <script src="contactform/contactform.js"></script>
+
+  <!-- Template Main Javascript File -->
+  <script src="js/main.js"></script>
+
+</body>
+</html>
+
+
+<script>  
+ $(document).ready(function(){  
+      $('#submit').click(function(){  
+           var username = $('#username').val();  
+           var email = $('#email').val();
+           var phone = $('#phone').val();  
+           var message = $('#message').val();  
+           if(username == '' || email == '' || phone == '' || message == '')  
+           {  
+                $('#error_message').html("All Fields are required");  
+           }  
+           else  
+           {  
+                $('#error_message').html('');  
+                $.ajax({  
+                     url:"insert.php",  
+                     method:"POST",  
+                     data:{username:username, email:email, phone:phone, message:message},  
+                     success:function(data){  
+                          $("form").trigger("reset");  
+                          $('#success_message').fadeIn().html(data);  
+                          setTimeout(function(){  
+                               $('#success_message').fadeOut("Slow");  
+                          }, 2000);  
+                     }  
+                });  
+           }  
+      });  
+ });  
+ </script>  
